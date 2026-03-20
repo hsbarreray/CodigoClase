8d1e5d32c2f401d2d2a7c51eb24599f15fe0b99e
santiagochiquiza <163699478+santiagochiquiza@users.noreply.github.com>
2026-03-20 18:21:46 +0000

    adelanto

--- codigo.java
+++ codigo.java
@@ -1,9 +1,74 @@
-public class codigo {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-    }
-}
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
