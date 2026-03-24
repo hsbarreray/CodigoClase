4939e4e7d0db57bdccc3171ae59e044232c1ee20
hsbarreray <hsbarreray@academia.usbbog.edu.co>
2026-03-24 14:52:54 +0000

    Adelanto

--- pedido.java
+++ pedido.java
@@ -1,3 +1,26 @@
-public class pedido {
-    
+public class Pedido {
+    private Cliente cliente;
+    private Producto producto;
+    private int cantidad;
+    private float total;
+ 
+    public Pedido(Cliente cliente, Producto producto, int cantidad) {
+        this.cliente = cliente;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.total = calcularTotal();
+    }
+ 
+    public float calcularTotal() {
+        return producto.getPrecio() * cantidad;
+    }
+ 
+    public Cliente getCliente() { return cliente; }
+    public void setCliente(Cliente cliente) { this.cliente = cliente; }
+    public Producto getProducto() { return producto; }
+    public void setProducto(Producto producto) { this.producto = producto; }
+    public int getCantidad() { return cantidad; }
+    public void setCantidad(int cantidad) { this.cantidad = cantidad; }
+    public float getTotal() { return total; }
 }
+ 
