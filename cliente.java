619cfc91da6a49f9549cdc8f4ac1c489bf710039
poiuytrfg <jcperezs1@academia.usbbog.edu.co>
2026-03-24 14:53:55 +0000

    mi parte

--- cliente.java
+++ cliente.java
@@ -1,3 +1,20 @@
 public class cliente {
-    
+    private String nombre;
+    private String identificacion;
+}
+public cliente (String nombre, String identificacion){
+    this.nombre = nombre;
+    this.identificacion = identificacion;
+}
+public String getNombre(){
+    return nombre;
+}
+public String setNombre(String nombre){
+    this.nombre = nombre;
+}
+public String getIdentificacion(){
+    return identificacion;
+}
+public String setIdentificacion(){
+    this.identificacion = identificacion;
 }
--- comida.java
+++ comida.java
@@ -1,3 +1,13 @@
-public class comida {
-    
+public class comida extends producto{
+    private String tipo;
+public comida(String tipo, String nombre, float precio){
+    super (nombre,precio);
+    this.tipo = tipo;
+}
+public String getTipo(){
+    return tipo;
+}
+public void setTipo(String tipo){
+    this.tipo = tipo;
+}
 }
