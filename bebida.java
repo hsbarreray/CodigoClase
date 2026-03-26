527b3579a39e88ae0a2ab94091ed00251f0d9b59
santiagochiquiza <163699478+santiagochiquiza@users.noreply.github.com>
2026-03-26 14:43:17 +0000

    Terminado

--- bebida.java
+++ bebida.java
@@ -1,3 +1,15 @@
 public class bebida extends producto {
-    
+    private String tamaño;
+
+    public bebida (String nombre, float precio ,String tamaño{
+        super(tamaño,precio);
+        this.tamaño = tamaño;
+    }
+    public String gettamaño(){
+
+        return tamaño;
+    }
+    public void settamaño(String tamaño) {
+        this.tamaño = tamaño;
+        }
 }
